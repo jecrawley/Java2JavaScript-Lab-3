98a2e13477af1677e1b732ad572c9ee2ff1ec3bd
James Crawley <jamescrawley@James-ZCW-15.local>
2016-09-30 12:23:29 -0400

    Allowed a single person to have many phone numbers.

--- js/script.js
+++ js/script.js
@@ -14,14 +14,26 @@ function listAllNames() {
 function listAllNumbers() {
     display.innerHTML = "";
     for (name in PhoneBook) {
-        display.innerHTML += PhoneBook[name] + "</br>";
+        PhoneBook[name].forEach(arrElements);
     }
 };
 
 function showAdd() {
   var name = prompt("Enter full name");
   var number = prompt("Enter phone number");
-  PhoneBook[name] = number;
+  var arr = PhoneBook[name] === undefined ? [] : PhoneBook[name];
+  arr.push(number);
+  PhoneBook[name] = arr;
+};
+
+function removeOne () {
+
+    var removeName = prompt ("Enter name of removal");
+    var removeNum = prompt("Enter number to remove");
+    var index = PhoneBook[removeName].indexOf(removeNum);
+
+    PhoneBook[removeName].splice(index, index + 1);
+
 };
 
 function showRemove() {
@@ -32,21 +44,33 @@ function showRemove() {
 function showLookup() {
   var lookup = prompt("Enter name to lookup");
   var lookupNum = "Name not found";
+
+  display.innerHTML = "";
   if (PhoneBook[lookup] !== undefined) {
       lookupNum = PhoneBook[lookup];
+      lookupNum.forEach(arrElements);
+  } else {
+      display.innerHTML = lookupNum;
   }
-  display.innerHTML = lookupNum;
+
+
 };
 
 function reverseLookup () {
     var lookup = prompt("Enter a number to lookup");
     var lookupName = "Number not found";
     for (var name in PhoneBook) {
-        if (PhoneBook[name] === lookup) {
+        if (PhoneBook[name].indexOf(lookup) !== -1) {
             lookupName = name;
         }
     }
     display.innerHTML = lookupName;
-}
+};
+
+function arrElements(element, index, array) {
+
+    display.innerHTML += element + "</br>";
+
+};
 
 var display = document.getElementById("display");
